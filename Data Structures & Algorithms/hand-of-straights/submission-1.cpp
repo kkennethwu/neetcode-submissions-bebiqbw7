d0b7854c33bd3ea803c63523ddf1d7e1eba68089
Kenneth Wu <71262989+kkennethwu@users.noreply.github.com>
2026-04-13 19:23:03 +0800

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        unordered_map<int, int> m;
+        int minHand = 1000;
+        int maxHand = 0;
+        for (const auto& h : hand) {
+            m[h]++;
+            minHand = min(minHand, h);
+            maxHand = max(maxHand, h);
+        }
+        // e.g. group size 4
+        // 0 0 0 3 3 3 4 1 1 1 0  1
+        // 0 1 2 3 4 5 6 7 8 9 10 1000
+        // 0 1 2 2 2 1
+
+        int left = minHand;
+        while (left + groupSize - 1 <= maxHand) {
+            int right = left + groupSize - 1;
+
+            int jumpIdx = right + 1;
+            for (int i = left + 1; i <= right; i++) {
+                if (m[i] < m[left]) return false;
+                else {
+                    if (m[i] > m[left]) {
+                        jumpIdx = min(jumpIdx, i);
+                    }
+                    m[i] -= m[left];
+                }
+            }
+            left = jumpIdx;
+        }
+
+        return left - 1 == maxHand ? true : false;
+    }
+};
